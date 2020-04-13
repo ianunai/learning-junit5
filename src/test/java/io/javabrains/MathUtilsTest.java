edd493d515e6e0e199c341c665d44ca0f9e38225
Anunai Ishan <ishananunai@gmail.com>
2020-04-13 15:22:09 -0400

    Implement assertAll

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -1,13 +1,23 @@
 package io.javabrains;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assumptions.*;
 
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MathUtilsTest {
 
+    MathUtils mathUtils;
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
     @Test
+    @DisplayName("Test Add Method")
     void testAdd() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual);
@@ -15,14 +25,21 @@ public class MathUtilsTest {
 
     @Test
     void testDivide() {
-        MathUtils mathUtils = new MathUtils();
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "divide method should throw" +
                 " an Arithmetic Exception when denominator is 0.");
     }
 
     @Test
+    void testMultiply() {
+        assertAll(
+                () -> assertEquals(4, mathUtils.multiply(2, 2)),
+                () -> assertEquals(0, mathUtils.multiply(2, 0)),
+                () -> assertEquals(-2, mathUtils.multiply(2, -1))
+        );
+    }
+
+    @Test
     void testComputeCircleArea() {
-        MathUtils mathUtils = new MathUtils();
         double expected = 314.1592653589793;
         double actual =  mathUtils.computeCircleArea(10);
         assertEquals(expected, actual);
